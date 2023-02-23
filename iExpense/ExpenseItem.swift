ef692c8e350bc97d6ef611e1313957b4d1197a3b
Koyko4 <283733694@qq.com>
2023-02-24 04:20:40 +0800

    Test 1

---
+++ iExpense/ExpenseItem.swift
@@ -0,0 +1,15 @@
+//
+//  ExpenseItem.swift
+//  iExpense
+//
+//  Created by Tao on 2023/2/23.
+//
+
+import Foundation
+
+struct ExpenseItem: Identifiable, Codable {
+    var id = UUID()
+    let name: String
+    let type: String
+    let amount: Double
+}
