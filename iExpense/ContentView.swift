a9757fc3818ea9de313455cc512bccd9061fee9d
Koyko4 <283733694@qq.com>
2023-02-24 06:22:24 +0800

    messed up

--- iExpense/ContentView.swift
+++ iExpense/ContentView.swift
@@ -5,47 +5,17 @@
 //  Created by Tao on 2023/2/23.
 //
 
-
 import SwiftUI
 
 struct ContentView: View {
-    @StateObject var expenses = Expenses()
-    @State private var showingAddExpense = false
-
     var body: some View {
-        NavigationView {
-            List {
-                ForEach(expenses.items) { item in
-                    HStack {
-                        VStack(alignment: .leading) {
-                            Text(item.name)
-                                .font(.headline)
-                            Text(item.type)
-                        }
-
-                        Spacer()
-
-                        Text(item.amount, format: .currency(code: "USD"))
-                    }
-                }
-                .onDelete(perform: removeItems)
-            }
-            .navigationTitle("iExpense")
-            .toolbar {
-                Button {
-                    showingAddExpense = true
-                } label: {
-                    Image(systemName: "plus")
-                }
-            }
-            .sheet(isPresented: $showingAddExpense) {
-                AddView(expenses: expenses)
-            }
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
         }
-    }
-
-    func removeItems(at offsets: IndexSet) {
-        expenses.items.remove(atOffsets: offsets)
+        .padding()
     }
 }
 
